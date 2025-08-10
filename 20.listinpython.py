049bf438bd7d7316bb77985f99b9d35ff64a5596
engrmianahsan <ahsanmeharofficial01@gmail.com>
2025-08-10 11:11:40 +0500

    using of list

---
+++ 20.listinpython.py
@@ -0,0 +1,20 @@
+# Program: List in Python
+# Example: List of student names
+
+studentName = ['ali', 'zain', 'mian', 'umer', 'ahmad']
+print(studentName [4]) 
+
+#now we learn about appendfunction
+studentName.append('usman')
+print(studentName)
+
+#append ka matlb k list k end per hum koi be  element ADD ker sakta han 
+
+#now we focus on pop function
+#studentName.pop()
+
+#ager hum  empty () breackets dayan gan to vo last vala element remove ker da ga or 
+# ager hum is ma index number dayn ga to vo vo vala number remove ker da ga 
+studentName.pop(3)  
+#ab hum na isa pop ko index number diya ha so ya 3 index j=ko hi romver ker ga 
+print(studentName)
