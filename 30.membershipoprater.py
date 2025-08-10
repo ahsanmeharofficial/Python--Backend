61158dd7c1f77b9e6b300a30b9d60e13fb7826c6
engrmianahsan <ahsanmeharofficial01@gmail.com>
2025-08-10 11:14:33 +0500

    member function

---
+++ 30.membershipoprater.py
@@ -0,0 +1,13 @@
+studentNames = ["Ahsan", "Bilal", "Usman", "Sana", "Hina"]
+
+# 'in' ka use:
+# Ye check karega ke "Ahsan" list me hai ya nahi
+if "Ahsan" in studentNames:
+    print("Ahsan class me hai.")
+
+# 'not in' ka use:
+# Ye check karega ke "Shahzad" list me nahi hai
+if "Shahzad" not in studentNames:
+    print("Shahzad class me nahi hai.")
+# len() ka use:
+# Ye list ke total elements count karta hai
