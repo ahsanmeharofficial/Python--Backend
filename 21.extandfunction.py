ab56dc5c15058f836a56a2a51f3679de18939ab9
engrmianahsan <ahsanmeharofficial01@gmail.com>
2025-08-10 11:11:59 +0500

    extand list

---
+++ 21.extandfunction.py
@@ -0,0 +1,8 @@
+firstList = ["apple", "banana"]
+secondList = ["mango", "orange"]
+
+# extend function ka use
+firstList.extend(secondList)
+
+print(firstList)
+# Output: ['apple', 'banana', 'mango', 'orange']
