23dc961d9b0387d46718e1fb06527b462feba41b
engrmianahsan <ahsanmeharofficial01@gmail.com>
2025-08-10 11:14:13 +0500

    len function

---
+++ 29.lengthchecking.py
@@ -0,0 +1,9 @@
+studentNames = ["Ahsan", "Bilal", "Usman", "Sana", "Hina"]
+
+# len() ka use:
+# Ye list ke total elements count karta hai
+totalStudents = len(studentNames)
+
+print("Total students:", totalStudents)  # Output: 5
+# Note: Agar list empty hai, to ye 0 return karega
+# Agar kisi specific element ki length chahiye, to wo bhi len() se kar sakte hain
