37b4c638bf9e6816a65998f835c2f775d3479490
engrmianahsan <ahsanmeharofficial01@gmail.com>
2025-08-10 11:13:15 +0500

    count method

---
+++ 25.countmothed.py
@@ -0,0 +1,8 @@
+fruitList = ["apple", "banana", "mango", "banana", "orange", "banana"]
+
+# count() ka use:
+# Ye batata hai ke ek value list mein kitni dafa aayi hai
+bananaCount = fruitList.count("banana")
+
+print("Banana kitni dafa aaya hai:", bananaCount)  # Output: 3
+# Note: Agar value list mein nahi hai, to ye 0 return karega
